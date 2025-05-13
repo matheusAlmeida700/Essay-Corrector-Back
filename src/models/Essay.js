9a626f6aa0dd03776a1f40fbbd99c45af90b98dd
Matheus Almeida <matheusauthentic8@gmail.com>
2025-05-12 21:17:38 -0300

    updated

--- src/models/Essay.js
+++ src/models/Essay.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const EssaySchema = new mongoose.Schema(
   {
     title: { type: String, required: false, maxlength: 500 },
-    theme: { type: String, required: true, minlength: 5, maxlength: 500 },
-    content: { type: String, required: true, minlength: 140, maxlength: 7000 },
+    theme: { type: String, required: true, maxlength: 500 },
+    content: { type: String, required: true, maxlength: 7000 },
     feedback: { type: String, required: false },
     correctedAt: { type: Date, default: Date.now },
     studentId: { type: mongoose.Schema.Types.ObjectId, required: true },
